9ffde1d8e4a6a9f7c8ea17e95d91ec46e02ad525
The Doge <alex.ren@gmail.com>
2023-09-27 22:48:01 -0700

    mapmind

--- src/mapmind.rs
+++ src/mapmind.rs
@@ -9,10 +9,7 @@ pub struct MapMind {}
 
 // App.MapMind$$get_Unit	7101dee2b0	App_Unit_o * App.MapMind$$get_Unit(App_MapMind_o * __this, MethodInfo * method)	12
 #[unity::from_offset("App", "MapMind", "get_Unit")]
-fn get_unit(
-    this: &MapMind,
-    method_info: OptionalMethod,
-) -> &'static Il2CppObject<Unit>;
+fn get_unit(this: &MapMind, method_info: OptionalMethod) -> &Unit;
 
 impl MapMind {
     pub fn get_instance() -> &'static mut MapMind {
