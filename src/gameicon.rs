6b4dbc01d22dfbdb56a875c8c01aee817c107eac
Tri Le <trilehuynh@gmail.com>
2025-08-16 12:22:53 -0400

    Dictionary Changes + SpriteAtlas Update

--- src/gameicon.rs
+++ src/gameicon.rs
@@ -1,6 +1,6 @@
 use unity::prelude::*;
 use unity::engine::Sprite;
-
+use unity::system::Dictionary;
 use crate::spriteatlasmanager::SpriteAtlasManager;
 
 #[unity::class("App", "GameIcon")]
@@ -11,22 +11,34 @@ impl GameIcon {
     pub fn try_get_system<'a>(icon_name: impl Into<&'a Il2CppString>) -> Option<&'static mut Sprite> {
         unsafe { gameicon_trygetsystem(icon_name.into(), None) }
     }
+    pub fn try_get_unit_icon_index<'a>(icon_name: impl Into<&'a Il2CppString>) -> Option<&'static mut Sprite> {
+        unsafe { gameicon_tryget_unit_icon(icon_name.into(), None) }
+    }
 }
 
-#[allow(dead_code)]
 pub struct GameIconStaticFields {
-    skill: SpriteAtlasManager,
-    item: SpriteAtlasManager,
-    efficacy: SpriteAtlasManager,
-    efficacy_outline: SpriteAtlasManager,
-    item_kinds: SpriteAtlasManager,
-    item_outline_kinds: SpriteAtlasManager,
-    god_symbol: SpriteAtlasManager,
-    god_ring: SpriteAtlasManager,
-    system: SpriteAtlasManager,
-    unit_icon_index: SpriteAtlasManager,
-    unit_icon_pallete: SpriteAtlasManager,
+    pub skill: &'static SpriteAlasManager,
+    pub item: &'static SpriteAlasManager,
+
+    pub efficacy: &'static SpriteAlasManager,
+    pub efficacy_outline: &'static SpriteAlasManager,
+
+    pub item_kinds: &'static SpriteAlasManager,
+    pub item_kinds_outline: &'static SpriteAlasManager,
+
+    pub god_symbol: &'static SpriteAlasManager,
+    pub god_ring: &'static SpriteAlasManager,
+
+    pub system: &'static SpriteAlasManager,
+    pub unit_icon_index: &'static SpriteAlasManager,
+    pub unit_icon_pallete: &'static SpriteAlasManager,
 }
+
+
+
+#[unity::from_offset("App", "GameIcon", "TyrGetUnitIconIndex")]
+extern "C" fn gameicon_tryget_unit_icon(icon_name: &Il2CppString, method_info: OptionalMethod) -> Option<&'static mut Sprite>;
+
 
 #[unity::from_offset("App", "GameIcon", "TryGetSystem")]
 extern "C" fn gameicon_trygetsystem(icon_name: &Il2CppString, method_info: OptionalMethod) -> Option<&'static mut Sprite>;
--- src/spriteatlasmanager.rs
+++ src/spriteatlasmanager.rs
@@ -1,6 +1,10 @@
+use unity::engine::Sprite;
+use unity::prelude::Il2CppString;
+use unity::system::Dictionary;
+
 #[unity::class("App", "SpriteAtlasManager")]
-pub struct SpriteAtlasManager {
-    handle: i32,
-    spriteatlas: i32,
-    cachetable: i32,
+pub struct SpriteAlasManager {
+    handle: *const u8,
+    sprite_atlas: *const u8,
+    pub cache_table: &'static Dictionary<'static, &'static Il2CppString, &'static Sprite>,
 }
